4509c98729a87c16589e8f8e481656dcdcd54b57
engg <75295603+Atoengg@users.noreply.github.com>
2022-09-10 10:27:51 +0700

    Add files via upload

---
+++ PELCOD PYTHON/main.py
@@ -0,0 +1,56 @@
+#belajar elemen dasar python
+
+
+print("hello world")
+
+bendaBerat = "laptop"
+
+print("saya mempunyai benda bernama"+bendaBerat)
+
+#type data di python: 1. String 2.int 3.float 4.Boolean
+
+#string
+String = '9'
+print(String)
+
+#int
+int = 10
+print("ini integer", + int)
+
+#tipe data float
+float = 10,5
+
+#boolean 
+#false dan true
+print("termasuk tipe data boolean =", + 10 > 9)
+
+a = 44
+b = 15
+
+print("termasuk tipe data boolean apa?", + a == b)
+
+#operator aritmatika
+# (+),(-),(*),(/),(**),(%)
+
+#penjumlahan
+a = 5
+b = 2
+
+print("hasil dari penjumlahan adalah = ",+ a+b)
+
+#pengurangan
+print("hasil dari pengurangan adalah = ",+ a - b)
+
+#perkalian
+print("hasil dari pengkalian adalah = ",+ a * b)
+
+#pembagian
+print("hasil dari pembagian adalah = ",+ a / b)
+
+#kuadrat
+print("hasil dari kuadrat adalah = ",+ a ** b)
+
+#modulus
+a = 7 
+b = 2
+print("hasil dari modulus adalah = ",+ a % b)
---
+++ PELCOD PYTHON/soal.py
@@ -0,0 +1,5 @@
+
+waktu = 5/6
+jarak = 30
+rumus = jarak/waktu
+print("kecepatan yang dibutuhkan", + rumus ,"km/jam")
